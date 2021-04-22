ceae640a3b80786976ab68b07fe208768e397ed3
EenMacFullstack <Iain.mcdulling@gmail.com>
2021-04-22 11:16:00 +0100

    first commit

---
+++ src/containers/CalculatorContainer.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+
+const MortgageCalculator = (() => {
+  
+
+
+
+
+
+
+    return(
+        <div>
+            <h1>Mortagage Calculator</h1>
+        </div>
+    )
+})
+
+export default MortgageCalculator;
